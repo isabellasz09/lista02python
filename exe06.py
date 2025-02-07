a3e0067da00dbf1e3837ad2fba87fd3daf345dc9
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-02-07 20:21:35 -0300

    atualização
    atv

--- exe06.py
+++ exe06.py
@@ -1,3 +1,4 @@
+# faça um programa que solicite o nome e em seguida
 nome= input("digite seu nome: ")
 sobrenome= input("digite seu sobrenome: ")
 print("olá", nome, sobrenome)
--- exe08.py
+++ exe08.py
@@ -1,5 +1,5 @@
 #escreva um programa que peça ao usuario para inserir dois numeros inteiros, some-os e exiba a mensagem: "o total é: [soma dos valores]"
-num1 = input ("digite o primeiro numero")
-num2 = input ("digite o segundo numero")
-resultado= num1*num2
+num1 = int (input ("digite o primeiro numero: "))
+num2 = int (input ("digite o segundo numero: "))
+resultado= num1+num2
 print (" O total é: ", resultado)
