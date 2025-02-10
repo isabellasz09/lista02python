6a13580ea7c69da6a86e034221156bc974476d5d
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-02-10 20:07:25 -0300

    atividade]

---
+++ exeaula2.py
@@ -0,0 +1,13 @@
+faturamento = 1000
+print('Para variavel faturamento que recebeu "1000" o tipo é: ',type(faturamento))
+
+faturamento = 1000.00
+print('Para variavel faturamento que recebeu "1000.00" o tipo é: ',type(faturamento))
+
+faturamento = '1.000'
+print('Para variavel faturamento que recebeu "1.000" o tipo é: ',type(faturamento))
+
+ganha_bonus = True
+print('Para variavel ganha_bonus que recebeu "True" tipo é: ', type(ganha_bonus))
+
+print(dir(str))
---
+++ exeaula4.py
@@ -0,0 +1,4 @@
+primeiro_nome="Isabella C."
+sobrenome="Souza"
+nome_completo=primeiro_nome+" "+sobrenome
+print("Olá, "+nome_completo.title()+"!")
